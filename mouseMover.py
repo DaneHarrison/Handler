77e1e71b806d777d7feca5cc3e9c00ed6718aa0d
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-02-25 16:31:15 -0600

    pywin32

--- mouseMover.py
+++ mouseMover.py
@@ -1,7 +1,8 @@
-import pyautogui
-
+import win32api, win32con
 def mouseMove(x,y):
-    pyautogui.moveTo(x,y)
+    win32api.SetCursorPos((x,y))
 
 def mouseClick(x,y):
-    pyautogui.click(x,y)
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
