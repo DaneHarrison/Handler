93ab79285b58734e304be342807597ab6a46bfd6
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-02-25 14:24:18 -0600

    click debounce

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import time
 import cv2
 import mediapipe as mp
 from handTracker import handTracker
@@ -12,6 +13,9 @@ FRAMEWIDTH = 640
 FRAMEHEIGHT = 480
 MONITORWIDTH = 1920
 MONITORHEIGHT = 1080
+DEBOUNCETIME = 2
+LASTCLICK = 0
+CURRCLICK = 0
 
 # def scale to monitor size
 def scaleToMonitor(xpos, ypos):
@@ -20,6 +24,8 @@ def scaleToMonitor(xpos, ypos):
     return x,y
 
 def main():
+    global LASTCLICK
+    global CURRCLICK
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAMEWIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAMEHEIGHT)
@@ -32,19 +38,21 @@ def main():
         lmList = tracker.positionFinder(image)
         mpHands = tracker.getHandsModule()
         if len(lmList) != 0:
-            #track the tip of the index finger to move the mouse
+            #print(lmList[mpHands.HandLandmark.INDEX_FINGER_TIP])
             _,xpos,ypos = lmList[mpHands.HandLandmark.INDEX_FINGER_TIP]
             scaledX, scaledY = scaleToMonitor(xpos, ypos)
             mouseMove(int(scaledX), int(scaledY))
 
-            # Find the distance to determine if a left click action should be done
             _,x1,y1 = lmList[mpHands.HandLandmark.THUMB_TIP]
             _,x2,y2 = lmList[mpHands.HandLandmark.MIDDLE_FINGER_PIP]
             distance = math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
             print(distance)
             if distance < 50:
-                print("click")
-                mouseClick(int(scaledX), int(scaledY))
+                CURRCLICK = time.time()
+                #print("click")
+                if CURRCLICK - LASTCLICK > DEBOUNCETIME:
+                    LASTCLICK = CURRCLICK
+                    mouseClick(int(scaledX), int(scaledY))
             
         cv2.imshow("Video",image)
         cv2.waitKey(1)
